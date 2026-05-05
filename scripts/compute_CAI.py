18d61ef09a363fac7083463c021c51b197dff4e6
Liam Shaw <liam.philip.shaw@gmail.com>
2026-05-05 16:11:08 +0100

    Lucks (2008) codon weights and CAI

---
+++ scripts/compute_CAI.py
@@ -0,0 +1,47 @@
+# Script to compute Codon Adaptation Index 
+
+import math
+from collections import defaultdict
+from Bio.Data import CodonTable
+import pandas as pd
+from Bio import SeqIO
+from collections import Counter
+import sys
+import glob
+
+def codon_usage(seq):
+    '''Codon usage for a given sequence.'''
+    codons = [str(seq[i:i+3]) for i in range(0, len(seq), 3)
+              if len(seq[i:i+3]) == 3]
+    return Counter(codons)
+
+def compute_cai(counts, weights):
+    '''CAI as defined by Sharp and Li (1987) and used by Lucks et al. (2008)'''
+    total_codons = sum(counts.values())
+    log_sum = 0
+    for codon, count in counts.items():
+        if codon in weights and weights[codon] > 0:
+            log_sum += count * math.log(weights[codon])
+    return math.exp(log_sum / total_codons)
+
+lucks_weights = pd.read_csv('../data/lucks_2008_ecoli_codon_weights.tsv', sep='\t', comment='#')
+weights_dict = dict(zip(lucks_weights["codon"], lucks_weights["w"])) 
+
+pattern = sys.argv[1]     
+output_file = sys.argv[2] 
+
+fasta_files = glob.glob(pattern)
+
+with open(output_file, "w") as out:
+    out.write("gene_id\tcai\n")
+    for fasta in fasta_files:
+        print(fasta)
+        for record in SeqIO.parse(fasta, "fasta"):
+            usage = codon_usage(record.seq)
+            cai = compute_cai(usage, weights_dict)
+            # include filename to avoid ID collisions
+            out.write(f"{fasta}|{record.id}\t{cai:.6f}\n")
+
+
+
+
